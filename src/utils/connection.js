d1c1d84642f563999e4bace1974fa20f799209b0
Suzair0318 <khansuzair1@gmail.com>
2025-07-08 13:43:18 +0500

    MongoDB Databse Deploy

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
-
+const {envPath} = require("../config/env");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    let connectionString;
+    
+    if (envPath === '.env.dev') {
+      connectionString = process.env.MONGODB_URI;
+    } else {
+      connectionString = process.env.MONGO_DB_LOCAL;
+    }
+
+    await mongoose.connect(connectionString);
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.log(error)
