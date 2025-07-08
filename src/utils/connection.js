720789bb1878f4383fea7aa1824ee0d35c75b272
Suzair0318 <khansuzair1@gmail.com>
2025-07-08 14:19:54 +0500

    Connection  Chnage

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -4,15 +4,18 @@ const {envPath} = require("../config/env");
 const connectDB = async () => {
   try {
     let connectionString;
+    let  test_messgae;
     
     if (envPath === '.env.dev') {
       connectionString = process.env.MONGODB_URI;
+      test_messgae = "MongoDB connected successfully! Development";
     } else {
       connectionString = process.env.MONGO_DB_LOCAL;
+      test_messgae = "MongoDB connected successfully! Production";
     }
 
     await mongoose.connect(connectionString);
-    console.log("MongoDB connected successfully!");
+    console.log(test_messgae);
   } catch (error) {
     console.log(error)
     console.error("MongoDB connection failed:", error.message);
