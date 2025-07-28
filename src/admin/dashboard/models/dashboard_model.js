1a8efcdeeb138439c8d9d5de9f6f670fe8b6a02e
Suzair0318 <khansuzair1@gmail.com>
2025-07-28 14:32:45 +0500

    Fix : Investor API

--- src/admin/dashboard/models/dashboard_model.js
+++ src/admin/dashboard/models/dashboard_model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const DashboardSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  organization: { type: String, required: true },
+  organization: { type: String},
 }, { timestamps: true });
 
 module.exports = mongoose.model('Dashboard', DashboardSchema);
