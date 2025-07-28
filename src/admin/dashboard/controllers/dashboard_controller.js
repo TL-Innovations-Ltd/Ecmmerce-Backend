2a55cb37ec9e60713b1441d8b7cabe881c2e91e7
Suzair0318 <khansuzair1@gmail.com>
2025-07-28 14:30:31 +0500

    Fix :  Investor API

--- src/admin/dashboard/controllers/dashboard_controller.js
+++ src/admin/dashboard/controllers/dashboard_controller.js
@@ -3,7 +3,7 @@ const dashboardService = require('../services/dashboard_service');
 exports.createDashboard = async (req, res) => {
   try {
     const { name, email, organization } = req.body;
-    if (!name || !email || !organization) {
+    if (!name || !email) {
       return res.status(400).json({ message: 'All fields are required.' });
     }
     const dashboard = await dashboardService.createDashboard({ name, email, organization });
